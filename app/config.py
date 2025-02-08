50697fa31cc5c339f5929a1bf98c9ac016522df8
kurasen <854597621@qq.com>
2025-02-08 16:20:53 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,13 +1,28 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+from dotenv import load_dotenv
 
 
 # 定义基础配置类
 class Config:
+    """通用配置类"""
+    # Flask 配置
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     LOCK_KEY = os.getenv('LOCK_KEY', 'user_login_lock')  # 默认值为 'user_login_lock'
     LOCK_EXPIRE = int(os.getenv('LOCK_EXPIRE', 300))  # 默认过期时间为 300 秒
+
+    # 数据库配置
+    @staticmethod
+    def get_sqlalchemy_uri():
+        db_username = os.getenv('DB_USERNAME', 'root')
+        db_password = os.getenv('DB_PASSWORD', '123123')
+        db_host = os.getenv('DB_HOST', '127.0.0.1')
+        db_port = os.getenv('DB_PORT', '3306')
+        db_name = os.getenv('DB_NAME', 'crop_al_hub')
+
+        return f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
 
     @staticmethod
     def init_app(app):
@@ -16,25 +31,20 @@ class Config:
 
 # 开发环境配置
 class DevelopmentConfig(Config):
-    dotenv_path = find_dotenv()
-    if dotenv_path:
-        load_dotenv(dotenv_path)
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('DB_USERNAME', 'root')}:"
-        f"{os.getenv('DB_PASSWORD', '123123')}@"
-        f"{os.getenv('DB_HOST', '127.0.0.1')}:{os.getenv('DB_PORT', '3306')}/"
-        f"{os.getenv('DB_NAME', 'crop_al_hub')}"
-    )
+    """开发环境配置"""
+    SQLALCHEMY_DATABASE_URI = Config.get_sqlalchemy_uri()
+
+    @staticmethod
+    def init_app(app):
+        """开发环境的初始化逻辑"""
+        # 加载 .env 文件
+        load_dotenv(Path('.') / '.env')
 
 
 # 生产环境配置
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('DB_USERNAME')}:"
-        f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/"
-        f"{os.getenv('DB_NAME')}"
-    )
+    """生产环境配置"""
+    SQLALCHEMY_DATABASE_URI = Config.get_sqlalchemy_uri()
 
 
 # 配置映射
