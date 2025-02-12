ce16a9257c6368989b9beaa455e741b6f45a23a1
kurasen <854597621@qq.com>
2025-02-12 17:56:44 +0800

    Update

---
+++ app/blueprint/utils/JSONEncoder.py
@@ -0,0 +1,21 @@
+# 自定义 JSONEncoder，处理 Decimal 类型
+import json
+from decimal import Decimal
+
+from flask import Response
+
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)  # 将 Decimal 转换为 float
+        return super().default(obj)
+
+
+# 创建一个通用的 JSON 响应方法
+def create_json_response(data):
+    # 使用自定义 JSONEncoder 来序列化 JSON
+    response = json.dumps(data, ensure_ascii=False, sort_keys=False, cls=CustomJSONEncoder)
+
+    # 返回构建好的 Response 对象
+    return Response(response, content_type='application/json')
