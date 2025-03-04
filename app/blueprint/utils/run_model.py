0e64fba2aeabbb5209de6e28cbf7846d7f83bb50
Cary <chenwanyue@Carydebijibendiannao.local>
2025-03-04 16:43:09 +0800

    update run_model

--- app/blueprint/utils/run_model.py
+++ app/blueprint/utils/run_model.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 CONNE = {
     'host':'10.0.4.71',
     'user':'chenwanyue',
-    'rsa':'/Users/chenwanyue/.ssh/rsa100471',#替换成自己的rsa文件路径
+    'rsa':'/Users/chenwanyue/.ssh/rsa100471',
     'info':'chenwanyue@10.0.4.71',
     'workspace':'/home/chenwanyue/workspace/'
 }
@@ -26,7 +26,7 @@ def run_command_remote(command):
     except Exception as e:
         logging.error(f"发生未知错误: {e}")
 
-# 在服务器上运行程序
+# 在本地运行程序
 def run_command_local(command):
     try:
         result = subprocess.run(command, shell=True, capture_output=True, text=True, check=True)
@@ -35,12 +35,68 @@ def run_command_local(command):
         print(f"命令执行失败，错误信息：{e.stderr}")
         logging.error(f"命令执行失败，错误信息：{e.stderr}")
 
+def clean_file(run_func, files):
+    clean_cmd = f"sudo rm -r {files}"
+    logging.debug(clean_cmd)
+    run_func(clean_cmd)
+
 # 获取模型运行的文件输出        
 def get_output(result_dir, output_file):
     cat_cmd = f"cat {result_dir}/{output_file}"
     ssh_command = f"ssh -i {CONNE['rsa']} {CONNE['info']} {cat_cmd}"
     result = subprocess.run(ssh_command, shell=True, capture_output=True, text=True, check=True)
     return result.stdout
+
+# 上传数据集到远程服务器
+def upload_database_remote(remote_path,local_path):    
+    mkdir_cmd =f"mkdir -p {remote_path}"
+    run_command_remote(mkdir_cmd)
+
+    #上传文件
+    scp_command = f"scp -i {CONNE['rsa']} -r {local_path} {CONNE['info']}:{remote_path}"
+    result = subprocess.run(scp_command, shell=True, capture_output=True, text=True)
+    print(result)
+
+# 从远程服务器下载一张图片
+def download_img_remote(remote_file_path,local_path):
+    scp_command = f"scp -i {CONNE['rsa']} {CONNE['info']}:{remote_file_path} {local_path}"
+    result = subprocess.run(scp_command, shell=True, capture_output=True, text=True)
+    print(result)
+
+# 上传一张图到远程服务器
+def upload_img_remote(remote_path,file_path):    
+    mkdir_cmd =f"mkdir -p {remote_path}"
+    run_command_remote(mkdir_cmd)
+
+    #上传文件
+    scp_command = f"scp -i {CONNE['rsa']} {file_path} {CONNE['info']}:{remote_path}"
+    result = subprocess.run(scp_command, shell=True, capture_output=True, text=True)
+    print(result)
+
+# 模拟的图像处理函数
+def run_models_in_container(run_func, data_path, model_image, instruction):
+    only_tag = f"User1_{model_image}"
+
+    #创建输出文件夹
+    result_dir = f"{data_path}/{only_tag}"
+    mkdir_cmd =f"mkdir -p {result_dir}"
+    logging.debug(mkdir_cmd)
+    run_func(mkdir_cmd)
+
+    #运行容器
+    docker_cmd = f"sudo docker run --name {only_tag}\
+        --mount type=bind,source={result_dir},target=/result\
+        --mount type=bind,source={data_path},target=/data\
+        {model_image} python3 main.py -i /data -o /result {instruction}"
+    logging.debug(docker_cmd)
+    run_func(docker_cmd)
+    
+    #清理环境
+    clean_cmd = f"sudo docker rm -f {only_tag}"
+    logging.debug(clean_cmd)
+    run_func(clean_cmd)
+    
+    return result_dir
 
 # 在数据集上运行模型
 def run_dataset_on_models_in_container(run_func, dataset_path, model_image, container_name, instruction, output_file):
@@ -72,8 +128,6 @@ def run_dataset_on_models_in_container(run_func, dataset_path, model_image, cont
     run_func(clean_cmd)
     return output
 
- 
-
 if __name__ == "__main__":
     #output = run_dataset_on_models_in_container(run_command_remote,"detetcion-tassel/dataset/test","detetcion-tassel","mytestCary","", "detection_results.txt")
     #output = run_dataset_on_models_in_container(run_command_remote,"segmentation-tassel/dataset/test","segmentation-tassel","mytestCary","", "segmentation_results.txt")
@@ -81,5 +135,10 @@ if __name__ == "__main__":
     #output = run_dataset_on_models_in_container(run_command_remote,"detetcion-seed-leaf/dataset/test/uav","detetcion-seed-leaf","mytestCary","-m uavp", "plant_detection_summary.csv")
     #output = run_dataset_on_models_in_container(run_command_remote,"detetcion-seed-leaf/dataset/test/ng","detetcion-seed-leaf","mytestCary","-m ngl", "leaf_detection_summary.csv")
     #output = run_dataset_on_models_in_container(run_command_remote,"detetcion-seed-leaf/dataset/test/uav","detetcion-seed-leaf","mytestCary","-m uavl", "leaf_detection_summary.csv")
-    #print(output)
+
+
+    result_dir = run_models_in_container(run_command_remote, CONNE['workspace']+"segmentation-tassel/dataset/test", "segmentation-tassel", "")
+    output = get_output(result_dir, "segmentation_results.txt")
+    print(output)
+    clean_file(run_command_remote, result_dir)
     pass
